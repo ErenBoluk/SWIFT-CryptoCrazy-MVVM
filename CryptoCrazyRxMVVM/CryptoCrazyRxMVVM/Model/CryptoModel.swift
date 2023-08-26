fa6dcb71dc34b4b33445af6ca69bd2269dd2b075
Eren Bölük <erenboluk@hotmail.com>
2023-08-26 14:23:37 +0300

    Crypto Model Added

---
+++ CryptoCrazyRxMVVM/CryptoCrazyRxMVVM/Model/CryptoModel.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoModel.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by midDeveloper on 26.08.2023.
+//
+
+import Foundation
+
+struct Crypto : Decodable{
+    let currency : String
+    let price : String
+}
