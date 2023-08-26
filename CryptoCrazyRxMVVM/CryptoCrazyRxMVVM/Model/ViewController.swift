f9977b307bf659855e14cb83a14e666bc2e75028
Eren Bölük <erenboluk@hotmail.com>
2023-08-26 14:13:00 +0300

    base upload

---
+++ CryptoCrazyRxMVVM/CryptoCrazyRxMVVM/Model/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by midDeveloper on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var context = cell.defaultContentConfiguration()
+        context.text = "Crypto Currency"
+        context.secondaryText = "Curypto Price"
+        cell.contentConfiguration = context
+        
+        return cell
+    }
+}
+
